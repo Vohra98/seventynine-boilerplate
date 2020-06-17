52019ad6b01714cedef9c1da323d1bf5fc2c0e58
Muhammad Vohra <Muhammed.vohra.work@gmail.com>
2020-06-17 11:53:49 +0100

    updated package

--- src/Boilerplate.php
+++ src/Boilerplate.php
@@ -2,14 +2,7 @@
 
 namespace seventynine\Wordpress\Boilerplate;
 
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\DisableXmlRpc;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemoveEmojiScript;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemoveGenerator;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemovePingback;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemoveRsdLink;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemoveShortLink;
-use seventynine\Wordpress\Boilerplate\Core\Wordpress\RemoveWlwManifest;
-use seventynine\Wordpress\Boilerplate\Core\Timber\Defaults as TimberDefaults;
+use \Timber;
 
 class Boilerplate
 {
@@ -18,13 +11,30 @@ class Boilerplate
      */
     public static function bootstrap()
     {
-        DisableXmlRpc::run();
-        RemoveEmojiScript::run();
-        RemoveGenerator::run();
-        RemovePingback::run();
-        RemoveRsdLink::run();
-        RemoveShortLink::run();
-        RemoveWlwManifest::run();
-        TimberDefaults::run();
+        add_filter('xmlrpc_enabled', '__return_false');
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_styles', 'print_emoji_styles');
+        add_filter('the_generator', function () {
+            return '';
+        });
+        add_filter('xmlrpc_methods', function ($methods) {
+            unset($methods['pingback.ping']);
+            return $methods;
+        });
+
+        add_filter('wp_headers', function ($headers) {
+            unset($headers['X-Pingback']);
+            return $headers;
+        });
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wp_shortlink_wp_head');
+        remove_action('wp_head', 'wlwmanifest_link');
+        if (class_exists('Timber')) {
+            Timber::$cache = false;
+            Timber::$twig_cache = false;
+            Timber::$dirname = 'templates';
+        }
     }
 }
